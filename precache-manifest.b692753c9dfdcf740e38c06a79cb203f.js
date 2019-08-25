68d20b28b645d68ff331d511c1a0a87505cb9548
Craig Haber <you@example.com>
2019-08-24 20:03:35 -0400

    Updates

---
+++ precache-manifest.b692753c9dfdcf740e38c06a79cb203f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7964185be204c94f1c5ed5ac30428687",
+    "url": "/psychologist-quote-generator/index.html"
+  },
+  {
+    "revision": "acac6ce928bc5b600bfc",
+    "url": "/psychologist-quote-generator/static/css/main.87eed07c.chunk.css"
+  },
+  {
+    "revision": "986d29a927f69082cf6b",
+    "url": "/psychologist-quote-generator/static/js/2.aa7c335a.chunk.js"
+  },
+  {
+    "revision": "acac6ce928bc5b600bfc",
+    "url": "/psychologist-quote-generator/static/js/main.371b7159.chunk.js"
+  },
+  {
+    "revision": "85919df44c0fa2e36264",
+    "url": "/psychologist-quote-generator/static/js/runtime~main.4382b6d9.js"
+  }
+]);
